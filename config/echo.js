eeb1fa33fe4cdd207b49614bf54d879378adf503
Jonathan Perry <jonathanperry@modernpoacher.com>
2019-06-06 01:10:25 +0100

    Housekeeping

--- config/echo.js
+++ config/echo.js
@@ -2,11 +2,11 @@ require('./mock')()
 
 console.log('zashiki-react-redux')
 console.log('zashiki-react-redux',
-  require('../lib/app/actions'),
-  require('../lib/app/components'),
-  require('../lib/app/reducers'),
-  require('../lib/app/router'),
-  require('../lib/app/routes'),
-  require('../lib/app/store'),
-  require('../lib/app/transformers'))
+  !!require('../lib/app/actions'),
+  !!require('../lib/app/components'),
+  !!require('../lib/app/reducers'),
+  !!require('../lib/app/router'),
+  !!require('../lib/app/routes'),
+  !!require('../lib/app/store'),
+  !!require('../lib/app/transformers'))
 process.exit()
