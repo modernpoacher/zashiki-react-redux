40a210737a1143ebd354e70478e5de5f519bed55
Jonathan Perry <jonathanperry@modernpoacher.com>
2019-09-09 01:21:10 +0100

    Jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   bail: 1,
   verbose: true,
-  rootDir: './src'
+  rootDir: './src',
+  collectCoverage: true,
+  coverageDirectory: '../coverage'
 }
---
+++ src/app/transformers/__tests__/stages/index.test.js
@@ -0,0 +1,36 @@
+import {
+  alpha,
+  omega,
+  embark,
+  debark
+} from '@modernpoacher/zashiki-react-redux/app/transformers/stages'
+
+describe('@modernpoacher/zashiki-react-redux/app/transformers/stages', () => {
+  describe('`alpha`', () => {
+    it('is defined', () => {
+      expect(alpha)
+        .toBeDefined()
+    })
+  })
+
+  describe('`omega`', () => {
+    it('is defined', () => {
+      expect(omega)
+        .toBeDefined()
+    })
+  })
+
+  describe('`embark`', () => {
+    it('is defined', () => {
+      expect(embark)
+        .toBeDefined()
+    })
+  })
+
+  describe('`debark`', () => {
+    it('is defined', () => {
+      expect(debark)
+        .toBeDefined()
+    })
+  })
+})
