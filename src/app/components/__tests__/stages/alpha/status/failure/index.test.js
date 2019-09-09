0d4d715a3373ca84b2cf8482b07993f233887189
Jonathan Perry <jonathanperry@modernpoacher.com>
2019-09-09 17:19:00 +0100

    Jest Enzyme

--- src/app/components/__tests__/stages/alpha/status/failure/index.test.js
+++ src/app/components/__tests__/stages/alpha/status/failure/index.test.js
@@ -7,12 +7,36 @@ import Failure from '@modernpoacher/zashiki-react-redux/app/components/stages/al
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('@modernpoacher/zashiki-react-redux/app/components/stages/alpha/status/failure', () => {
-  it('renders', () => {
-    const component = (
-      <Failure name={'MOCK EXCEPTION'} onSubmit={jest.fn()} />
-    )
+  describe('Always', () => {
+    it('renders', () => {
+      const component = (
+        <Failure name={'MOCK NAME'} onSubmit={jest.fn()} />
+      )
 
-    expect(shallow(component))
-      .toMatchSnapshot()
+      expect(shallow(component))
+        .toMatchSnapshot()
+    })
+  })
+
+  describe('`code` is an integer', () => {
+    it('renders', () => {
+      const component = (
+        <Failure code={1} name={'MOCK NAME'} onSubmit={jest.fn()} />
+      )
+
+      expect(shallow(component))
+        .toMatchSnapshot()
+    })
+  })
+
+  describe('`message` is a string', () => {
+    it('renders', () => {
+      const component = (
+        <Failure name={'MOCK NAME'} message={'MOCK MESSAGE'} onSubmit={jest.fn()} />
+      )
+
+      expect(shallow(component))
+        .toMatchSnapshot()
+    })
   })
 })
