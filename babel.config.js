ea7cd4db88a67bfbc1c2a584ece877162f68a4ad
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-23 12:51:41 +0100

    `prepare` `install`

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,60 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        useBuiltIns: 'entry',
-        corejs: 3
-      }
-    ],
-    '@babel/react'
+const debug = require('debug')
+
+const log = debug('@modernpoacher/zashiki-react-redux')
+
+const {
+  env: {
+    DEBUG = '@modernpoacher/zashiki-react-redux',
+    NODE_ENV = 'development'
+  }
+} = process
+
+debug.enable(DEBUG)
+
+log('`@modernpoacher/zashiki-react-redux` is awake')
+
+const presets = [
+  [
+    '@babel/env', {
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      useBuiltIns: 'entry',
+      corejs: 3
+    }
   ],
-  plugins: [
-    '@babel/proposal-export-namespace-from',
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-class-properties',
-    [
-      'module-resolver', {
-        alias: {
-          '@modernpoacher/zashiki-react-redux': './src'
-        }
+  '@babel/react'
+]
+
+const plugins = [
+  '@babel/proposal-export-namespace-from',
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-class-properties',
+  [
+    'module-resolver', {
+      alias: {
+        '@modernpoacher/zashiki-react-redux': './src'
       }
-    ]
+    }
   ]
+]
+
+function using () {
+  log({ NODE_ENV })
+
+  return NODE_ENV === 'production'
+}
+
+module.exports = (api) => {
+  if (api) api.cache.using(using)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
